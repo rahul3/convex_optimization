0247d38a7a51d71e98d361af6c08f950e9ee96be
simghera <alexkakabadze21@gmail.com>
2025-03-24 12:29:39 -0400

    Update proximal_operators.py
    Added proximal of the iso norm

--- deblur_denoise/core/proximal_operators.py
+++ deblur_denoise/core/proximal_operators.py
@@ -1,7 +1,46 @@
 import torch
+import numpy as np
 
 def prox_l1(x: torch.Tensor, lambda_val: float) -> torch.Tensor:
     """
     Proximal operator of the L1 norm 
     """
     return torch.sign(x) * torch.clamp(torch.abs(x) - lambda_val, min=0)
+
+def prox_iso(x: torch.Tensor, lambda_val: float) -> torch.Tensor:
+    """
+    Proximal operator of the iso norm.
+
+    x = [x_1, x_2] where x_i are N x N matrices
+    """
+
+    # Extract x_1 and x_2 from the input tensor
+    x_1 = x[0, :, :]
+    x_2 = x[1, :, :]
+    
+    # Compute the squared sum of x_1 and x_2 element-wise
+    sum_squared = x_1**2 + x_2**2
+    
+    # Compute the element-wise square root of the sum of squares
+    norm = torch.sqrt(sum_squared)
+    
+    # Initialize the outputs with the same shape as x_1 and x_2
+    o_1 = torch.zeros_like(x_1)
+    o_2 = torch.zeros_like(x_2)
+
+    
+    # Apply the conditions
+    condition = norm > lambda_val
+    o_1[condition] = (1 - lambda_val / norm[condition]) * x_1[condition]
+    o_2[condition] = (1 - lambda_val / norm[condition]) * x_2[condition]
+    
+    # Return the result tensor
+    return torch.stack((o_1, o_2))
+
+
+# tests
+# t1
+x = torch.tensor([[[0, 3], [1, 2]],[[2, 1], [1, 1/2]]])
+l = 2
+
+print(prox_iso(x, 2))
